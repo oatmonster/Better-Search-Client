786a2668bfe3a1e5854fa748501617487b7d70d5
Alex Zhao <oatmonster@gmail.com>
2019-10-21 20:23:07 -0700

    Started item detail component

--- src/app/search.component.ts
+++ src/app/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 import { ApiService } from './api.service';
 
@@ -10,25 +12,25 @@ export class SearchComponent implements OnInit {
 
   items: any[];
 
-  constructor( private apiService: ApiService ) { };
-
   query;
 
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { };
+
   search( form ) {
-    console.log( form.query );
-    this.apiService.getItems( form.query ).subscribe( res => {
-      this.items = [];
-      this.items = res.findItemsByKeywordsResponse[ 0 ].searchResult[ 0 ].item || [];
-      this.items.forEach( function ( item ) { console.log( item.itemId[ 0 ] + item.galleryURL ) } );
-      console.log( this.items.length );
-    } )
+    this.router.navigateByUrl( 'search/' + form.query );
+    this.apiService.searchItems( form.query ).subscribe( res => {
+      this.items = res;
+    } );
   }
 
   ngOnInit() {
-    this.apiService.getItems( 'watches' ).subscribe( res => {
-      //console.log( JSON.stringify( res ) );
-      this.items = res.findItemsByKeywordsResponse[ 0 ].searchResult[ 0 ].item || [];
-      console.log( this.items[ 0 ] );
-    } )
+    this.query = this.activatedRoute.snapshot.params[ 'query' ];
+    this.apiService.searchItems( this.query ).subscribe( res => {
+      this.items = res;
+    } );
   }
 }
