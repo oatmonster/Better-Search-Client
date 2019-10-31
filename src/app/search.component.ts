a5a95122eb44e33c26655df1a9af4a72515a718c
oatmonster <alexzhaowa@gmail.com>
2019-10-31 12:50:32 -0700

    Made search bar sticky

--- src/app/search.component.ts
+++ src/app/search.component.ts
@@ -9,6 +9,7 @@ import { IQuery } from './api.service';
 @Component( {
   selector: 'search',
   templateUrl: 'search.component.html',
+  styleUrls: [ 'search.component.css' ]
 } )
 export class SearchComponent implements OnInit {
 
