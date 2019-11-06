8cd8c615c3c4aefe64e4f5e438d2ac863d994ec0
oatmonster <alexzhaowa@gmail.com>
2019-11-06 15:05:12 -0800

    Fixed auto imports

--- src/app/loading.interceptor.ts
+++ src/app/loading.interceptor.ts
@@ -1,8 +1,8 @@
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { finalize } from "rxjs/operators";
-import { LoadingService } from "./loading.service";
-import { Injectable } from "@angular/core";
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { LoadingService } from './loading.service';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
