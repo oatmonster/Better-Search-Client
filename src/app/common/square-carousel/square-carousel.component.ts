1c1d77c976a89664abecc392e53883d485fcd0dc
oatmonster <alexzhaowa@gmail.com>
2019-11-15 14:59:21 -0800

    Added square bootstrap gallery component

---
+++ src/app/common/square-carousel/square-carousel.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component( {
+  selector: 'square-carousel',
+  templateUrl: 'square-carousel.component.html',
+  styleUrls: [ 'square-carousel.component.css' ]
+} )
+export class SquareCarouselComponent implements OnInit {
+
+  @Input()
+  items: string[];
+
+  @Input()
+  thumbnails: boolean = false;
+
+  ngOnInit() {
+
+  }
+}
