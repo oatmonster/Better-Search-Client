3e4a9f07792868fd694e85d2c7548410a1851c54
Alex Zhao <oatmonster@gmail.com>
2019-11-20 10:09:38 -0800

    Updated loading bar to slowly auto increment

--- src/app/loading/loading.service.ts
+++ src/app/loading/loading.service.ts
@@ -2,28 +2,37 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class LoadingService {
-  private width: number;
-  private visible: boolean = false;
+  private width_: number;
+  private visible_: boolean = false;
+  private incTimeout_;
 
   start() {
-    this.width = 0;
-    this.visible = true;
+    this.width_ = 0;
+    this.visible_ = true;
   }
 
   complete() {
-    this.width = 100;
-    setTimeout( () => this.visible = false, 250 );
-  }
-
-  getVisible() {
-    return this.visible;
+    this.width_ = 100;
+    setTimeout( () => this.visible_ = false, 250 );
   }
 
   setWidth( width: number ) {
-    this.width = width;
+    this.width_ = width;
+    clearTimeout( this.incTimeout_ );
+    this.incTimeout_ = setTimeout( () => this.increment_(), 250 );
+  }
+
+  private increment_() {
+    if ( this.width_ >= 99 ) return;
+    let inc: number = Math.max( 100 / ( this.width_ + 10 ) - Math.random(), 0 );
+    this.setWidth( this.width_ + inc );
+  }
+
+  getVisible() {
+    return this.visible_;
   }
 
   getWidth() {
-    return this.width;
+    return this.width_;
   }
 }
